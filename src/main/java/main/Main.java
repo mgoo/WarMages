4ec3f6d713c1ce50f34b385577b4f542a912b67e
mgoo <andrewmcghienz@gmail.com>
2017-08-18 21:31:11 +1200

    Fixed current code style

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
 /**
- * Delete this when you put your assignments here
+ * Delete this when you put your assignments here.
  */
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("The program ran!");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("The program ran!");
+  }
 }
