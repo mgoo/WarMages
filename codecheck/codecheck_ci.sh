daaee53afe9399cec1712f7387f3c41215cf727d
mgoo <andrewmcghienz@gmail.com>
2017-08-18 20:23:10 +1200

    Changed path

--- codecheck/codecheck_ci.sh
+++ codecheck/codecheck_ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 OUTPUT=$(java -jar codecheck/checkstyle-8.1-all.jar -c codecheck/google_checks.xml src/ | grep WARN)
 if [ ! -z "$OUTPUT" -a "$OUTPUT" != " " ]; then
-    java -jar codecheck/checkstyle-8.1-all.jar -c codecheck/google_checks.xml src/
+    java -jar codecheck/checkstyle-8.1-all.jar -c codecheck/google_checks.xml ../src/
     false
 fi
 echo "Code Correctly Styled :D"
