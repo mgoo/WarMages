7f613f01c2cd93082cc0438d8e2300567d2c92ff
Dylan Chong <dylan.chong@icloud.com>
2017-08-18 15:43:12 +1200

    Change build configuration names

--- src/main/java/test/ExampleTest.java
+++ src/main/java/test/ExampleTest.java
@@ -2,12 +2,11 @@ package test;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 public class ExampleTest {
 
     @Test
     public void tempTest() {
+        System.out.println("Temporary test");
     }
 
 }
