6d17d4ebb719356f1591a15e8ac91088bd4b21f1
Dylan Chong <dylan.chong@icloud.com>
2017-08-19 22:38:44 +1200

    Put Main.java back

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,31 +2,7 @@ package main;
 
 public class Main {
 
-  class NonException {
-
-  }
-
-  // SHould throw IC_SUPERCLASS_USES_SUBCLASS_DURING_INITIALIZATION error ?
-  public static class CircularClassInitialization {
-
-    static class InnerClassSingleton extends CircularClassInitialization {
-
-      static InnerClassSingleton singleton = new InnerClassSingleton();
-    }
-
-    static CircularClassInitialization foo = InnerClassSingleton.singleton;
-  }
-
-  /**
-   * Stupid warning
-   */
   public static void main(String[] args) {
-    // Poor indenting
-    // Long line
     System.out.println("The program ran!");
   }
-
-  private void bla() {
-  }
-
 }
