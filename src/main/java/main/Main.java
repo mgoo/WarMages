41307e199db16f550a0ddd7d1cd0bb77ea9c784f
Dylan Chong <dylan.chong@icloud.com>
2017-08-19 13:14:17 +1200

    Added bad code style

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,21 @@ package main;
  */
 public class Main {
 
+  // Should throw warning about naming
+  class NonException{}
+
+  // SHould throw IC_SUPERCLASS_USES_SUBCLASS_DURING_INITIALIZATION error ?
+public static class CircularClassInitialization {
+    static class InnerClassSingleton extends CircularClassInitialization {
+        static InnerClassSingleton singleton = new InnerClassSingleton();
+    }
+
+    static CircularClassInitialization foo = InnerClassSingleton.singleton;
+}
+
   public static void main(String[] args) {
-    System.out.println("The program ran!");
+    // Poor indenting
+    // Long line
+      System.out.println("The program ran!****************************************************************************************************");
   }
 }
