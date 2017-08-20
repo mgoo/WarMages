790ec245da72d32e06bcf824e113b760ab3d969d
Dylan Chong <dylan.chong@icloud.com>
2017-08-20 18:50:11 +1200

    Add JavaDoc to Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,15 @@
 package main;
 
+/**
+ * Should just instantiate the needed classes to run the app. This should not contain any
+ * app logic.
+ */
 public class Main {
 
+  /**
+   * Start the app.
+   */
   public static void main(String[] args) {
-    System.out.println("The program ran!");
+
   }
 }
