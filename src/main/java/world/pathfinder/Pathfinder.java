8d483b93b5ede6b180aab241b48a2b11ba93bb27
arorahrsh <arorahrsh@myvuw.ac.nz>
2017-09-18 21:25:02 +1200

    move GameController to controller package

---
+++ src/main/java/world/pathfinder/Pathfinder.java
@@ -0,0 +1,14 @@
+package pathfinder;
+
+/**
+ * Implements the A* path finding algorithm to find the shortest path
+ * between two places on the map (as a list of {@link MapPoint}). Ignores
+ * other units/entities and a one-tile is passable by all units (they can
+ * go through it).
+ */
+public class PathFinder {
+  public static List<MapPoint> findPath(World world, MapPoint start, MapPoint end){
+    //TODO
+    throw new Error("NYI");
+  }
+}
