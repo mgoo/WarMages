5d32ea8326aaf15deb922cb2d9b60cdb645532f2
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-09-19 21:45:24 +1200

    Created Entity API and subclasses, as well as Ability. Added method stubs for all public methods.

---
+++ src/main/java/main/Entity/Ability.java
@@ -0,0 +1,10 @@
+package main.Entity;
+
+/**
+ * An ability can be applied to (a) Unit(s)
+ */
+public class Ability {
+  public void apply(Unit unit){
+    throw new Error("NYI");
+  }
+}
---
+++ src/main/java/main/Entity/Projectile.java
@@ -0,0 +1,21 @@
+package main.Entity;
+
+/**
+ * Projectile extends {@link Entity}. A projectile is fired by a unit at a
+ * target (another unit) and affects it in some way upon impact.
+ */
+public abstract class Projectile extends Entity {
+  private final Unit target;
+
+  public Projectile(Unit target){
+    throw new Error("NYI");
+  }
+
+  @Override
+  public GameImage.Config getImage(){
+    throw new Error("NYI");
+  }
+
+  public abstract void hits(Unit unit);
+}
+
