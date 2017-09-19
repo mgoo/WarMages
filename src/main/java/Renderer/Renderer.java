50607d31dad97d0e54329ae2f25c9b3570e899a7
diputagabr@ecs.vuw.ac.nz <diputagabr@myvuw.ac.nz>
2017-09-19 22:49:26 +1200

    Added javadocs for the method stubs and classes Renderable and Renderer

--- src/main/java/Renderer/Renderer.java
+++ src/main/java/Renderer/Renderer.java
@@ -2,6 +2,11 @@ package Renderer;
 
 import view.GameView;
 
+/**
+ * Renders all renderables onto a canvas and supplies the Renderable interface.
+ * Ideally it will use OpenGL to take advantage of hardware acceleration.
+ * This class should also be reponsible for looping.
+ */
 public abstract class Renderer {
   public Renderer(GameView gameView){}
 }
