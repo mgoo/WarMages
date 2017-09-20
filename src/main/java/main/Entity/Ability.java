3cf2d61a8480ca3ce83b206e8955c4c970932812
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-09-20 20:44:58 +1200

    Added getIconImage and getInfo method stubs in Ability

--- src/main/java/main/Entity/Ability.java
+++ src/main/java/main/Entity/Ability.java
@@ -7,4 +7,10 @@ public class Ability {
   public void apply(Unit unit){
     throw new Error("NYI");
   }
+
+  public GameImage.Config getIconImage(){
+    throw new Error("NYI");
+  }
+
+  public String getInfo(){ throw new Error("NYI");}
 }
