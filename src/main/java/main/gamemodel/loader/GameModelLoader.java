ac262eabf16bd1536c43fd76603ff6c3feec4e63
Dylan Chong <dylan.chong@icloud.com>
2017-09-20 22:42:46 +1200

    Add files

--- src/main/java/main/gamemodel/loader/GameModelLoader.java
+++ src/main/java/main/gamemodel/loader/GameModelLoader.java
@@ -1,7 +1,12 @@
 package main.gamemodel.loader;
 
 import java.io.IOException;
+import world.World;
 
+/**
+ * Creates a new [@link GameModel} and it's required {@link World}, along with {@link Entity}
+ * objects in the default positions in the {@link World}.
+ */
 public class GameModelLoader {
   public GameModelLoader(FileLoader fileLoader) {
     throw new Error("NYI");
---
+++ src/main/java/test/GameModelLoaderTest.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * TODO move this into a correct test package
+ */
+public class GameModelLoaderTest {
+
+}
