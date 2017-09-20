964a4e4b6fa68e0cfa7539deacfd317231ce0915
diputagabr@ecs.vuw.ac.nz <diputagabr@myvuw.ac.nz>
2017-09-21 11:20:23 +1200

    Added selection methods onto the gameModel allowing other APIs to interact with the world API

--- src/main/java/gamemodel/GameModel.java
+++ src/main/java/gamemodel/GameModel.java
@@ -1,5 +1,7 @@
 package gamemodel;
 
+import util.MapRect;
+
 /**
  * A container class for all model components (including units, items,
  * projectiles, etc). This also handles the progression of the story/game
@@ -27,4 +29,31 @@ public class GameModel {
   public void startGame() {
     throw new Error("NYI");
   }
+
+  /**
+   * A setter method to select a collection
+   * @param selection points on the world that may contain entities
+   */
+  public void setEntitySelection(MapRect selection){
+    throw new Error("NYI");
+  }
+
+  /**
+   * A getter method get a previously selected collection
+   * @return a collection of selected entities
+   */
+  public Collection<Entity> getEntitySelection() {
+    throw new Error("NYI");
+  }
+
+  /**
+   * A getter method which sets the selected collection and
+   * returns a collection selected entities
+   * @param selection points on the world that may contain entities
+   * @return a collection of selected entities
+   */
+  public Collection<Entity> getEntitySelection(MapRect selection) {
+    throw new Error("NYI");
+  }
+
 }
