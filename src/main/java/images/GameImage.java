d042d7e282522ac1bc91b29bab233dc6f7b66250
Dylan Chong <dylan.chong@icloud.com>
2017-09-20 21:51:26 +1200

    Write GameImage

--- src/main/java/images/GameImage.java
+++ src/main/java/images/GameImage.java
@@ -1,21 +1,53 @@
 package images;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 /**
  * Reference all of the image files in the app here by adding a new enum value
  *
  * @see ImageProvider for loading the images
  */
 public enum GameImage {
-  // These are just placeholder values / files. We will add more later
-  ONE("one.png"),
-  TWO("two.png");
+
+  // Values used in tests only
+  _TEST_FULL_SIZE("one.png"),
+  _TEST_PARTIAL_SIZE("two.png", 1, 1, 3, 2);
+
+  private static final int MAX_SIZE = Integer.MAX_VALUE;
+
+  public final String filename;
+  public final int startX;
+  public final int startY;
+  public final int width;
+  public final int height;
 
   GameImage(String filename) {
-    throw new Error("NYI");
+    this(filename, 0, 0, MAX_SIZE, MAX_SIZE);
   }
 
   GameImage(String filename, int startX, int startY, int width, int height) {
-    throw new Error("NYI");
+    this.filename = filename;
+    this.startX = startX;
+    this.startY = startY;
+    this.width = width;
+    this.height = height;
+  }
+
+  public BufferedImage load(ImageProvider imageProvider) throws IOException {
+    BufferedImage fromCache = imageProvider.getFromCache(this);
+    if (fromCache != null) {
+      return fromCache;
+    }
+
+    BufferedImage image = imageProvider.load(filename);
+    image = clipToBounds(image);
+
+    imageProvider.storeInCache(this, image);
+    return image;
+  }
+
+  private BufferedImage clipToBounds(BufferedImage image) {
+    return image.getSubimage(startX, startY, width, height);
   }
 }
-
---
+++ src/main/java/test/images/GameImageTest.java
@@ -0,0 +1,59 @@
+package test.images;
+
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import images.GameImage;
+import images.ImageProvider;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import org.junit.Test;
+
+public class GameImageTest {
+
+  private final BufferedImage testImage = new BufferedImage(10, 5, TYPE_INT_ARGB);
+
+  {
+    testImage.setRGB(2, 1, Color.BLUE.getRGB());
+  }
+
+  private final ImageProvider imageProvider = new ImageProvider() {
+    @Override
+    protected BufferedImage load(String filename) throws IOException {
+      return testImage;
+    }
+
+    @Override
+    protected void storeInCache(GameImage gameImage, BufferedImage image) {
+
+    }
+
+    @Override
+    protected BufferedImage getFromCache(GameImage gameImage) {
+      return null;
+    }
+  };
+
+  @Test
+  public void load_noSizeSpecified_returnsEntireImage() throws IOException {
+    BufferedImage image = GameImage._TEST_FULL_SIZE.load(imageProvider);
+
+    assertEquals(image.getWidth(), testImage.getWidth());
+    assertEquals(image.getHeight(), testImage.getHeight());
+  }
+
+  @Test
+  public void load_sizeSpecified_returnsSubImage() throws IOException {
+    GameImage gameImage = GameImage._TEST_PARTIAL_SIZE;
+    BufferedImage image = gameImage.load(imageProvider);
+
+    // Assume default colours are not blue
+    assertNotEquals(testImage.getRGB(1, 0), Color.WHITE.getRGB());
+
+    assertEquals(testImage.getRGB(1, 0), Color.BLUE.getRGB());
+    assertEquals(gameImage.width, image.getWidth());
+    assertEquals(gameImage.height, image.getHeight());
+  }
+}
