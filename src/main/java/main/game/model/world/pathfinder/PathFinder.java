04513a420522efaf88f8c23b7b9aaef320adf6c6
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-09-22 15:56:50 +1200

    Basic A* path finding implemented

--- src/main/java/main/game/model/world/pathfinder/PathFinder.java
+++ src/main/java/main/game/model/world/pathfinder/PathFinder.java
@@ -1,6 +1,11 @@
 package main.game.model.world.pathfinder;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 import main.game.model.world.World;
 import main.util.MapPoint;
 
@@ -14,7 +19,37 @@ import main.util.MapPoint;
 public class PathFinder {
 
   public static List<MapPoint> findPath(World world, MapPoint start, MapPoint end) {
-    //TODO
-    throw new Error("NYI");
+    PriorityQueue<AStarNode> fringe = new PriorityQueue<AStarNode>();
+    fringe.add(new AStarNode(start, null, 0, estimate(start, end)));
+
+    Set<MapPoint> visited = new HashSet<MapPoint>();
+
+    while(!fringe.isEmpty()){
+      AStarNode tuple = fringe.poll();
+
+      if(!visited.contains(tuple.currentPoint)){
+        visited.add(tuple.currentPoint);
+
+        if(tuple.currentPoint == end) {
+          return tuple.pathTaken;
+        }
+
+        for(MapPoint neigh: tuple.currentPoint.getNeighbours()){
+
+          if(!visited.contains(neigh)){
+            double costToNeigh = tuple.costFromStart + tuple.currentPoint.distance(neigh);
+            double estTotal = costToNeigh + estimate(neigh, end);
+            List<MapPoint> neighPath = new ArrayList<MapPoint>(tuple.pathTaken);
+            neighPath.add(neigh);
+            fringe.add(new AStarNode(neigh, tuple.currentPoint, costToNeigh, estTotal, neighPath));
+          }
+        }
+      }
+    }
+    return new ArrayList<MapPoint>();
+  }
+
+  private static double estimate(MapPoint current, MapPoint goal){
+    return current.distance(goal);
   }
 }
