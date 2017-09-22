2a3f776ec7ff8c6c4c95027e0a05d8d38ec93169
Andrew McGhie <mcghieandr@ecs.vuw.ac.nz>
2017-09-22 21:22:56 +1200

    Revert "Started writing code"
    This reverts commit 0b92ab90f3ef0542bf3c3e48762c39d8b6281c22

--- src/main/java/main/game/view/GameView.java
+++ src/main/java/main/game/view/GameView.java
@@ -1,18 +1,12 @@
 package main.game.view;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
 import main.game.controller.GameController;
 import main.game.model.GameModel;
-import main.game.model.entity.Entity;
 import main.renderer.Renderable;
 
 /**
- * A View of the Game. Is responsible for creating all the EntityRenderable objects and keeping their
+ * A View of the Game. Is responsible for creating all the Renderable objects and keeping their
  * locations uptodate with the locations in the models.
  *
  * @author Andrew McGhie
@@ -22,56 +16,13 @@ public class GameView {
   private final GameController gameController;
   private final GameModel gameModel;
 
-  private Collection<EntityRenderable> renderablesCache =
-      new PriorityQueue<>(new RenderableComparator());
-
   public GameView(GameController gameController, GameModel gameModel) {
     this.gameController = gameController;
     this.gameModel = gameModel;
-
   }
 
   public Collection<Renderable> getRenderables() {
     // TODO
     return null;
-  }
-
-  private void updateRenderables() {
-    final Set<EntityRenderable> toRemove = new HashSet<>();
-    final Set<Entity> enitiesToCheck = new HashSet<>(this.gameModel.getAllEntities());
-
-    EntityRenderable.lastTickTime = System.currentTimeMillis();
-    EntityRenderable.nextTickTime = EntityRenderable.lastTickTime + 20;
-
-    this.renderablesCache.forEach(renderable -> {
-      if (!enitiesToCheck.contains(renderable.getEntity())) {
-        toRemove.add(renderable);
-      } else {
-        enitiesToCheck.remove(renderable.getEntity());
-      }
-    });
-    toRemove.forEach(renderable -> {
-      this.renderablesCache.remove(renderable);
-    });
-
-    enitiesToCheck.forEach(entity -> {
-      this.renderablesCache.add(new EntityRenderable(entity));
-    });
-
-    this.renderablesCache.forEach(entityRenderable -> {
-      entityRenderable.update();
-    });
-
-
-  }
-
-
-  private class RenderableComparator implements Comparator<Renderable> {
-
-    @Override
-    public int compare(Renderable r1, Renderable r2) {
-      return (int) ((r1.getEffectiveEntityPosition().x + r1.getEffectiveEntityPosition().y) -
-                (r2.getEffectiveEntityPosition().x + r2.getEffectiveEntityPosition().y));
-    }
   }
 }
