341e2bdca686e614da580727b142f6ab23e35714
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-09-22 15:29:19 +1200

    Added TODO note to isPassable for later

--- src/main/java/main/game/model/world/World.java
+++ src/main/java/main/game/model/world/World.java
@@ -29,6 +29,8 @@ public class World {
   /**
    * A getter method which checks if a certain point in the map can be moved into.
    *
+   * TODO - make sure that the method returns false for points outside the Map
+   *
    * @param point a point in the map.
    * @return returns whether the point can be moved into.
    */
