f3374e24b4cc897ab66fb07ab1d856aa02e01e46
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-09-22 17:29:13 +1200

    Add equals and hashcode

--- src/main/java/main/util/MapSize.java
+++ src/main/java/main/util/MapSize.java
@@ -12,4 +12,32 @@ public class MapSize {
     this.width = width;
     this.height = height;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    MapSize mapSize = (MapSize) o;
+
+    if (Double.compare(mapSize.width, width) != 0) {
+      return false;
+    }
+    return Double.compare(mapSize.height, height) == 0;
+  }
+
+  @Override
+  public int hashCode() {
+    int result;
+    long temp;
+    temp = Double.doubleToLongBits(width);
+    result = (int) (temp ^ (temp >>> 32));
+    temp = Double.doubleToLongBits(height);
+    result = 31 * result + (int) (temp ^ (temp >>> 32));
+    return result;
+  }
 }
