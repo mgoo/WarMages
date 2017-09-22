10ab6ec4daaf75fa56d7bc76bbb059d48c5c091c
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-09-22 15:50:52 +1200

    Updated MapPoint with distance() and getNeighbours()

--- src/main/java/main/util/MapPoint.java
+++ src/main/java/main/util/MapPoint.java
@@ -1,6 +1,7 @@
 package main.util;
 
 import java.awt.Point;
+import java.util.List;
 
 public class MapPoint {
 
@@ -15,5 +16,15 @@ public class MapPoint {
   public MapPoint(Point p) {
     this.x = p.x;
     this.y = p.y;
+  }
+
+  public double distance(MapPoint other) {
+    //TODO Hrshikesh
+    return 0.0;
+  }
+
+  public List<MapPoint> getNeighbours(){
+    //TODO Hrshikesh
+    return null;
   }
 }
