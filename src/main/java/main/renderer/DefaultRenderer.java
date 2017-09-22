a8c78f02f1bfe7eff54354742c78344792927f02
diputagabr@ecs.vuw.ac.nz <diputagabr@myvuw.ac.nz>
2017-09-22 15:13:09 +1200

    created loop and then set viewImage image to get gameView images

---
+++ src/main/java/main/renderer/DefaultRenderer.java
@@ -0,0 +1,12 @@
+package main.renderer;
+
+import main.game.view.GameView;
+
+public class DefaultRenderer extends Renderer{
+
+
+  public DefaultRenderer(GameView gameView) {
+    super(gameView);
+  }
+
+}
