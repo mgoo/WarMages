faecf2a35976065b74741cb0778243d235b7b96d
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-09-25 17:19:30 +1300

    Added MovableEntity to accommodate for entities that require pathfinding. Unit now extends MovableEntity which extends Entity. Changed tick method in unit to accomodate for this. Tick method in MovableEntity takes care of changing position with each tick.

---
+++ src/main/java/main/game/model/entity/MovableEntity.java
@@ -0,0 +1,44 @@
+package main.game.model.entity;
+
+import java.util.List;
+import main.util.MapPoint;
+import main.util.MapSize;
+
+public abstract class MovableEntity extends Entity {
+  protected List<MapPoint> path;
+  protected int speed;
+
+  /**
+   * Constructor takes the position of the entity and the size.
+   *
+   * @param position = position of Entity
+   * @param size = size of Entity
+   */
+  public MovableEntity(MapPoint position, MapSize size) {
+    super(position, size);
+  }
+
+  /**
+   * Sets the path to be followed by the unit to the given path.
+   * @param path
+   */
+  public void setPath(List<MapPoint> path){
+    this.path=path;
+  }
+
+  @Override
+  public void tick(long timeSinceLastTick) {
+    long distToBeTravelled = speed*timeSinceLastTick;
+    int leway = 5; //todo finalize
+    //update position
+    if(path!=null&&!path.isEmpty()){
+      for(MapPoint mp : path){
+        double distFromCurrent = Math.sqrt((Math.pow(mp.x-position.x, 2)+Math.pow(mp.y-position.y, 2)));
+        if(distFromCurrent<distToBeTravelled+leway&&distFromCurrent>distToBeTravelled-leway){
+          position = mp;
+          return;
+        }
+      }
+    }
+  }
+}
