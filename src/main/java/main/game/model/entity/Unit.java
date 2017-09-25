df002a17e2495ef0d2b354e9c95094fb0379bacb
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-09-25 17:19:30 +1300

    Added MovableEntity to accommodate for entities that require pathfinding. Unit now extends MovableEntity which extends Entity. Changed tick method in unit to accomodate for this. Tick method in MovableEntity takes care of changing position with each tick.

--- src/main/java/main/game/model/entity/Unit.java
+++ src/main/java/main/game/model/entity/Unit.java
@@ -11,7 +11,7 @@ import main.util.MapSize;
  * Unit extends{@link Entity}. A unit is a part of a team, specified by an enum colour. It has
  * health, and can attack other team units.
  */
-public class Unit extends Entity implements Damageable, Attackable {
+public class Unit extends MovableEntity implements Damageable, Attackable {
   public static int startingHealth = 200; //todo refactor for special starting health per unit
   protected int health;
   protected final Team team;
@@ -21,7 +21,7 @@ public class Unit extends Entity implements Damageable, Attackable {
   protected UnitType unitType;
   protected UnitState unitState;
   protected List<GameImage> images;
-  protected int imagesIdx, damageAmount, speed;
+  protected int imagesIdx, damageAmount;
 
   /**
    * Constructor takes the unit's position, size, and team.
@@ -32,19 +32,23 @@ public class Unit extends Entity implements Damageable, Attackable {
   public Unit(MapPoint position, MapSize size, Team team, UnitSpriteSheet sheet, UnitType unitType) {
     super(position, size);
     this.team = team;
+    this.unitType = unitType;
     isDead = false;
     health = startingHealth;
     speed = startingSpeed;
-    spriteSheet=sheet;
+    damageAmount = baselineDamage;
+    spriteSheet = sheet;
     images = new ArrayList<>();
-    this.unitType = unitType;
     unitState = UnitState.DEFAULT_STATE;
     unitState.setDirection(Direction.LEFT);
     images = unitType.getImagesFor(unitState, spriteSheet);
     imagesIdx = 0;
-    damageAmount = baselineDamage;
   }
 
+  /**
+   * Sets the damage amount of this Unit's attack to the given amount. Must be 0 < amount < 100
+   * @param amount
+   */
   public void setDamageAmount(int amount){
     assert amount>0&&amount<100;
     damageAmount=amount;
@@ -70,33 +74,53 @@ public class Unit extends Entity implements Damageable, Attackable {
     imagesIdx = 0;
   }
 
-  public void moveTowards(Point targetPoint){
-
-  }
-
   @Override
   public void tick(long timeSinceLastTick) {
+    MapPoint oldPosition = position;
     //update image
     if(imagesIdx == images.size()-1){
       //reset state back to default
       setStateTo(UnitState.DEFAULT_STATE);
     }
     image=images.get(imagesIdx);
-    //update position
-    switch(unitState.getDirection()){
-      case UP:
-        moveY(-1*speed*timeSinceLastTick);
-        return;
-      case DOWN:
-        moveY(speed*timeSinceLastTick);
-        return;
-      case LEFT:
-        moveX(-1*speed*timeSinceLastTick);
-        return;
-      case RIGHT:
-        moveX(speed*timeSinceLastTick);
-        return;
-      default:
+    super.tick(timeSinceLastTick);
+    //set direction of state based on movement gradient.
+    updateDirection(oldPosition);
+  }
+
+  /**
+   * Sets direction of Unit based on x and y coordinate differences between the given oldPosition and the current position.
+   * @param oldPosition
+   */
+  private void updateDirection(MapPoint oldPosition){
+    if(position.x<oldPosition.x){ //moved left
+      if(position.y<oldPosition.y){ //moved left and up
+        if(Math.abs(position.y-oldPosition.y)<Math.abs(position.x-oldPosition.x)){ //greater change in y
+          unitState.setDirection(Direction.UP);
+        } else { //greater change in x
+          unitState.setDirection(Direction.LEFT);
+        }
+      } else { //moved left and down
+        if(Math.abs(position.y-oldPosition.y)<Math.abs(position.x-oldPosition.x)){ //greater change in y
+          unitState.setDirection(Direction.DOWN);
+        } else { //greater change in x
+          unitState.setDirection(Direction.LEFT);
+        }
+      }
+    } else { //moved right
+      if(position.y<oldPosition.y){ //moved right and up
+        if(Math.abs(position.y-oldPosition.y)<Math.abs(position.x-oldPosition.x)){ //greater change in y
+          unitState.setDirection(Direction.UP);
+        } else { //greater change in x
+          unitState.setDirection(Direction.RIGHT);
+        }
+      } else { //moved right and down
+        if(Math.abs(position.y-oldPosition.y)<Math.abs(position.x-oldPosition.x)){ //greater change in y
+          unitState.setDirection(Direction.DOWN);
+        } else { //greater change in x
+          unitState.setDirection(Direction.RIGHT);
+        }
+      }
     }
   }
 
