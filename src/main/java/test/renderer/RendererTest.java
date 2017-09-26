5d12bcd67e250c1ee1c6bd6d636847e114c3a92d
diputagabr <diputagabr@rhythm.ecs.vuw.ac.nz>
2017-09-27 09:46:51 +1300

    Created test class and first test stub

---
+++ src/main/java/test/renderer/RendererTest.java
@@ -0,0 +1,18 @@
+package test.renderer;
+
+import main.renderer.Renderer;
+import org.junit.Test;
+
+/**
+ * Created by diputagabr on 27/09/17.
+ * Test classes which focuses on testing the Renderer
+ * At the moment it will only mock the view and model to draw it
+ */
+public class RendererTest {
+
+  @Test
+  public void checkIfRendererDrawsEntities(){
+    //TODO
+  }
+
+}
