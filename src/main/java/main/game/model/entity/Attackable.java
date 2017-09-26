88e5e321f47daf3c1bf80632f3c30d34bbdd4e12
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-09-26 22:40:55 +1300

    fixed linting

--- src/main/java/main/game/model/entity/Attackable.java
+++ src/main/java/main/game/model/entity/Attackable.java
@@ -7,6 +7,7 @@ import main.util.MapSize;
  * Attackables can attack units.
  */
 public abstract class Attackable extends MovableEntity {
+
   public static final int baselineDamage = 5;
   public static final int leeway = 5;
   protected Unit target;
@@ -25,25 +26,28 @@ public abstract class Attackable extends MovableEntity {
 
   /**
    * Attacks the given unit.
+   *
    * @param unit to be attacked
    */
   abstract void attack(Unit unit);
 
   /**
    * Set's the Unit's target to the given Unit.
+   *
    * @param target to be attacked
    */
-  public void setTarget(Unit target){
-    this.target=target;
+  public void setTarget(Unit target) {
+    this.target = target;
   }
 
   /**
    * Sets the damage amount of this Unit's attack to the given amount. Must be 0 < amount < 100.
+   *
    * @param amount of damage to deal to target.
    */
-  public void setDamageAmount(int amount){
-    assert amount>0 && amount<100;
-    damageAmount=amount;
+  public void setDamageAmount(int amount) {
+    assert amount > 0 && amount < 100;
+    damageAmount = amount;
   }
 
   /**
@@ -56,12 +60,14 @@ public abstract class Attackable extends MovableEntity {
   }
 
   /**
-   * Returns boolean whether the distance between the target and the Attackable is less than the leway.
+   * Returns boolean whether the distance between the target and the Attackable is less than the
+   * leway.
    *
    * @return boolean representing distance less than leeway.
    */
-  public boolean checkTargetWithinProximity(){
-    if(Math.sqrt(Math.pow(target.getPosition().x - position.x, 2) + Math.pow(target.getPosition().y - position.y, 2))<leeway){
+  public boolean checkTargetWithinProximity() {
+    if (Math.sqrt(Math.pow(target.getPosition().x - position.x, 2) + Math
+        .pow(target.getPosition().y - position.y, 2)) < leeway) {
       attack(target);
       return true;
     }
