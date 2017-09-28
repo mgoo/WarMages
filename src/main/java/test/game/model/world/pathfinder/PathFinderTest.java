44499592011136fc6878d43bdd6b988e94c41d7d
arorahrsh <arorahrsh@myvuw.ac.nz>
2017-09-29 10:34:39 +1300

    Updated pathfinder tests

--- src/main/java/test/game/model/world/pathfinder/PathFinderTest.java
+++ src/main/java/test/game/model/world/pathfinder/PathFinderTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import main.game.model.world.World;
 import main.game.model.world.pathfinder.PathFinder;
 import main.util.MapPoint;
@@ -14,14 +15,9 @@ public class PathFinderTest {
 
   @Test
   public void test01_testHorizontalPath() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        return true;
-      }
-    };
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {return true;};
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(3, 1));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(3, 1));
     List<MapPoint> expected = new ArrayList<>(Arrays.asList(mp(2, 1), mp(3, 1)));
 
     assertEquals(expected, actual);
@@ -29,14 +25,9 @@ public class PathFinderTest {
 
   @Test
   public void test02_testVerticalPath() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        return true;
-      }
-    };
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {return true;};
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(1, 3));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(1, 3));
     List<MapPoint> expected = new ArrayList<>(Arrays.asList(mp(1, 2), mp(1, 3)));
 
     assertEquals(expected, actual);
@@ -44,14 +35,9 @@ public class PathFinderTest {
 
   @Test
   public void test03_testDiagonalPath() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        return true;
-      }
-    };
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {return true;};
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(3, 3));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(3, 3));
     List<MapPoint> expected = new ArrayList<>(Arrays.asList(mp(2, 2), mp(3, 3)));
 
     assertEquals(expected, actual);
@@ -59,14 +45,9 @@ public class PathFinderTest {
 
   @Test
   public void test04_testDiagonalPath() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        return true;
-      }
-    };
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {return true;};
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(5, 5));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(5, 5));
     List<MapPoint> expected = new ArrayList<>(
         Arrays.asList(mp(2, 2), mp(3, 3), mp(4, 4), mp(5, 5)));
 
@@ -75,17 +56,14 @@ public class PathFinderTest {
 
   @Test
   public void test06_testPathWithObstacle() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        if (point.y == 2 && (point.x == 2 || point.x == 3 || point.x == 4)) {
-          return false;
-        }
-        if (point.y == 1 && point.x == 4) {
-          return false;
-        }
-        return true;
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {
+      if (mapPoint.y == 2 && (mapPoint.x == 2 || mapPoint.x == 3 || mapPoint.x == 4)) {
+        return false;
       }
+      if (mapPoint.y == 1 && mapPoint.x == 4) {
+        return false;
+      }
+      return true;
     };
 
     //  12345
@@ -96,7 +74,7 @@ public class PathFinderTest {
 
     //S = start; G = goal; x = obstacle; + = free space
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(5, 2));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(5, 2));
     List<MapPoint> expected = new ArrayList<>(
         Arrays.asList(mp(2, 1), mp(3, 0), mp(4, 0), mp(5, 1), mp(5, 2)));
 
@@ -105,17 +83,14 @@ public class PathFinderTest {
 
   @Test
   public void test07_testPathWithObstacle() {
-    World world = new World(null, null) {
-      @Override
-      public boolean isPassable(MapPoint point) {
-        if (point.y == 2 && (point.x == 2 || point.x == 3 || point.x == 4)) {
-          return false;
-        }
-        if ((point.y == 1 || point.y == 0) && point.x == 4) {
-          return false;
-        }
-        return true;
+    Function<MapPoint, Boolean> isPassable = mapPoint -> {
+      if (mapPoint.y == 2 && (mapPoint.x == 2 || mapPoint.x == 3 || mapPoint.x == 4)) {
+        return false;
       }
+      if ((mapPoint.y == 1 || mapPoint.y == 0) && mapPoint.x == 4) {
+        return false;
+      }
+      return true;
     };
 
     //  12345
@@ -126,7 +101,7 @@ public class PathFinderTest {
 
     //S = start; G = goal; x = obstacle; + = free space
 
-    List<MapPoint> actual = PathFinder.findPath(world, mp(1, 1), mp(5, 2));
+    List<MapPoint> actual = PathFinder.findPath(isPassable, mp(1, 1), mp(5, 2));
     List<MapPoint> expected = new ArrayList<>(
         Arrays.asList(mp(1, 2), mp(2, 3), mp(3, 3), mp(4, 3), mp(5, 2)));
 
