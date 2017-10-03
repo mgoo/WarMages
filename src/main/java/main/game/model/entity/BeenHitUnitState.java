78f5cea9bd685184558946c4b3bf49b4c0aadf14
Dylan Chong <dylan.chong@icloud.com>
2017-10-04 09:46:26 +1300

    Make the unit state take a unit

--- src/main/java/main/game/model/entity/BeenHitUnitState.java
+++ src/main/java/main/game/model/entity/BeenHitUnitState.java
@@ -1,14 +1,13 @@
 package main.game.model.entity;
 
-import main.images.UnitSpriteSheet;
 import main.images.UnitSpriteSheet.Sequence;
 
 public class BeenHitUnitState extends UnitState {
 
   private static final long serialVersionUID = 1L;
 
-  public BeenHitUnitState(Direction direction, UnitSpriteSheet sheet) {
-    super(Sequence.HURT, direction, sheet);
+  public BeenHitUnitState(Direction direction, Unit unit) {
+    super(Sequence.HURT, direction, unit);
   }
 
   @Override
