9eab0517516e00b733b6226abaa4fe9430debe39
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-10-05 00:16:25 +1300

    remove getNeighbours from MapPoint class

--- src/main/java/main/util/MapPoint.java
+++ src/main/java/main/util/MapPoint.java
@@ -79,24 +79,4 @@ public class MapPoint implements Serializable {
   public double distance(MapPoint other) {
     return Math.hypot(this.x - other.x, this.y - other.y);
   }
-
-  /**
-   * Returns the neighbouring MapPoints of this MapPoint. This is achieved by hardcoding the
-   * neighbours in a list and returning that list.
-   *
-   * @return the list of neighbours
-   */
-  public List<MapPoint> getNeighbours() {
-    return new ArrayList<MapPoint>(
-        Arrays.asList(
-            new MapPoint(this.x - 1, this.y), //left
-            new MapPoint(this.x + 1, this.y), //right
-            new MapPoint(this.x, this.y - 1), //top
-            new MapPoint(this.x, this.y + 1), //bottom
-            new MapPoint(this.x - 1, this.y - 1), //top-left
-            new MapPoint(this.x + 1, this.y - 1), //top-right
-            new MapPoint(this.x - 1, this.y + 1), //bottom-left
-            new MapPoint(this.x + 1, this.y + 1) //bottom-right
-        ));
-  }
 }
