81be72612ac43f285cc012e26964bfd400caf607
Dylan Chong <dylan.chong@icloud.com>
2017-10-05 11:28:23 +1300

    Minor tidying

--- src/main/java/main/menu/MainMenu.java
+++ src/main/java/main/menu/MainMenu.java
@@ -19,7 +19,7 @@ public class MainMenu extends Menu {
   private final WorldSaveModel worldSaveModel;
 
   /**
-   * Injects the decencies.
+   * Injects the dependencies.
    */
   public MainMenu(Main main,
                   WorldLoader worldLoader,
@@ -29,11 +29,14 @@ public class MainMenu extends Menu {
     super(main);
     this.worldLoader = worldLoader;
     this.worldSaveModel = worldSaveModel;
-    this.menuController = new MainMenuController(this.main,
-        this, worldLoader,
+    this.menuController = new MainMenuController(
+        this.main,
+        this,
+        worldLoader,
         worldSaveModel,
         imageView,
-        config);
+        config
+    );
   }
 
   @Override
