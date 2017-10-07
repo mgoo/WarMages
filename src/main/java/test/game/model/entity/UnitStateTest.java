fe61a2a7d66ca6373b5ebfb4cd233847b7c0718a
Andrew McGhie <andrewmcghienz@gmail.com>
2017-10-07 15:34:11 +1300

    Wrote some test

---
+++ src/main/java/test/game/model/entity/UnitStateTest.java
@@ -0,0 +1,64 @@
+package test.game.model.entity;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import main.game.model.Level;
+import main.game.model.entity.HeroUnit;
+import main.game.model.entity.Team;
+import main.game.model.entity.Unit;
+import main.game.model.entity.UnitType;
+import main.game.model.entity.usable.Ability;
+import main.game.model.world.World;
+import main.images.DefaultImageProvider;
+import main.images.GameImageResource;
+import main.images.UnitSpriteSheet;
+import main.util.MapPoint;
+import main.util.MapSize;
+import org.junit.Before;
+import org.junit.Test;
+import test.game.model.world.WorldTestUtils;
+
+/**
+ * TODO atm units dont move so its a bit hard to test
+ * @author Andrew McGhie
+ */
+public class UnitStateTest {
+
+  @Test
+  public void testUnitState() {
+    Unit unit = new Unit(new MapPoint(0,0),
+        new MapSize(100, 100),
+        Team.PLAYER,
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER);
+    HeroUnit heroUnit = new HeroUnit(new MapPoint(50,50),
+        new MapSize(100, 100),
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER,
+        new ArrayList<Ability>());
+    List<MapPoint> path = new ArrayList<>();
+    path.add(new MapPoint(1,1));
+    path.add(new MapPoint(2,2));
+
+    unit.setPath(path);
+
+    List<Level> levels = new ArrayList<>();
+    levels.add(WorldTestUtils.createLevelWith(
+        unit,
+        heroUnit
+    ));
+    World world = new World(levels, heroUnit);
+    unit.tick(50, world);
+    unit.tick(50, world);
+    unit.tick(50, world);
+    BufferedImage img;
+    try {
+      img = unit.getImage().load(new DefaultImageProvider());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
--- src/main/java/test/game/model/entity/UnitTest.java
+++ src/main/java/test/game/model/entity/UnitTest.java
@@ -2,18 +2,25 @@ package test.game.model.entity;
 
 import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import main.game.model.GameModel;
+import main.game.model.Level;
 import main.game.model.entity.HeroUnit;
 import main.game.model.entity.Team;
 import main.game.model.entity.Unit;
 import main.game.model.entity.UnitType;
+import main.game.model.entity.usable.Ability;
+import main.game.model.entity.usable.Effect;
 import main.game.model.world.World;
 import main.images.GameImageResource;
 import main.images.UnitSpriteSheet;
@@ -138,4 +145,104 @@ public class UnitTest {
     }
 
   }
+
+  @Test
+  public void testTickEffects() {
+    Unit unit = new Unit(new MapPoint(0,0),
+        new MapSize(100, 100),
+        Team.PLAYER,
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER);
+    HeroUnit heroUnit = new HeroUnit(new MapPoint(50,50),
+        new MapSize(100, 100),
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER,
+        new ArrayList<Ability>());
+
+    List<Level> levels = new ArrayList<>();
+    levels.add(WorldTestUtils.createLevelWith(unit, heroUnit));
+    World world = new World(levels, heroUnit);
+
+    Ability ability1 = new Ability("", GameImageResource.TEST_IMAGE_1_1.getGameImage(), 1, 2) {
+      @Override
+      public Effect _createEffectForUnit(Unit unit) {
+        return new Effect(unit, 1) {
+          @Override
+          public int alterDamageAmount(int currentDamageAmount) {
+            return currentDamageAmount + 1;
+          }
+        };
+      }
+    };
+    Ability ability2 = new Ability("", GameImageResource.TEST_IMAGE_1_1.getGameImage(), 1, 2) {
+      @Override
+      public Effect _createEffectForUnit(Unit unit) {
+        return new Effect(unit, 2) {
+          @Override
+          public int alterDamageAmount(int currentDamageAmount) {
+            return currentDamageAmount + 1;
+          }
+        };
+      }
+    };
+    Ability ability3 = new Ability("", GameImageResource.TEST_IMAGE_1_1.getGameImage(), 1, 2) {
+      @Override
+      public Effect _createEffectForUnit(Unit unit) {
+        return new Effect(unit, 1) {
+          @Override
+          public int alterDamageAmount(int currentDamageAmount) {
+            return currentDamageAmount + 1;
+          }
+        };
+      }
+    };
+
+    assertEquals(5, unit.getDamageAmount());
+    ability1.useOnUnits(Arrays.asList(unit));
+    assertEquals(6, unit.getDamageAmount());
+    ability2.useOnUnits(Arrays.asList(unit));
+    assertEquals(7, unit.getDamageAmount());
+    ability3.useOnUnits(Arrays.asList(unit));
+    assertEquals(8, unit.getDamageAmount());
+
+    for (int i = 0; i < 25; i++) {
+      unit.tick(50, world);
+    }
+    assertEquals(6, unit.getDamageAmount());
+  }
+
+  @Test
+  public void testEntityAttack() {
+    Unit playerUnit = new Unit(new MapPoint(0,0),
+        new MapSize(0.5, 0.5),
+        Team.PLAYER,
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER);
+    Unit enemyUnit = new Unit(new MapPoint(1,1),
+        new MapSize(0.5, 0.5),
+        Team.ENEMY,
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.SPEARMAN);
+    HeroUnit heroUnit = new HeroUnit(new MapPoint(50,50),
+        new MapSize(0.5, 0.5),
+        new UnitSpriteSheet(GameImageResource.MALE_MAGE_SPRITE_SHEET),
+        UnitType.ARCHER,
+        new ArrayList<Ability>());
+
+    List<Level> levels = new ArrayList<>();
+    levels.add(WorldTestUtils.createLevelWith(playerUnit, enemyUnit, heroUnit));
+    World world = new World(levels, heroUnit);
+
+    playerUnit.setTarget(enemyUnit, world);
+
+    int previousHealth = enemyUnit.getHealth();
+
+    for (int i = 0; i < 100; i++) {
+      world.tick(50);
+    }
+
+    assertNotEquals(enemyUnit.getHealth(), previousHealth);
+  }
+
+
 }
