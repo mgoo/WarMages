e023d5cf494cf97c19fe0e6e9f52cc7364f1c089
Dylan Chong <dylan.chong@icloud.com>
2017-10-11 21:45:27 +1300

    Move Usable.use implementation into BaseUsable

---
+++ src/main/java/main/game/model/entity/usable/BaseUsable.java
@@ -0,0 +1,54 @@
+package main.game.model.entity.usable;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Collection;
+import main.game.model.entity.Unit;
+import main.game.model.entity.exceptions.CantApplyToUnitsException;
+import main.game.model.entity.exceptions.UsableStillInCoolDownException;
+import main.game.model.world.World;
+
+/**
+ * All {@link Usable}s should extend {@link BaseUsable}.
+ */
+public interface BaseUsable extends Usable {
+
+  default void use(World world, Collection<Unit> selectedUnits) {
+    if (!isReadyToBeUsed()) {
+      throw new UsableStillInCoolDownException();
+    }
+
+    Collection<Unit> unitsToApplyOn = _selectUnitsToApplyOn(
+        requireNonNull(world),
+        requireNonNull(selectedUnits)
+    );
+
+    for (Unit unit : unitsToApplyOn) {
+      Effect effect = _createEffectForUnit(unit);
+      unit.addEffect(effect);
+    }
+
+    _startCoolDown();
+  }
+
+  /**
+   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS!
+   * Pick what units to apply this ability to.
+   *
+   * @param selectedUnits The units that are currently selected by the user.
+   * @throws CantApplyToUnitsException When there is a unit that we cannot apply this {@link Usable}
+   *     to.
+   */
+  Collection<Unit> _selectUnitsToApplyOn(World world, Collection<Unit> selectedUnits);
+
+  /**
+   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS! Starts the cool-down period.
+   */
+  void _startCoolDown();
+
+  /**
+   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS! Creates a new effect. Does not need to check
+   * {@link Usable#isReadyToBeUsed()}.
+   */
+  Effect _createEffectForUnit(Unit unit);
+}
--- src/main/java/main/game/model/entity/usable/Usable.java
+++ src/main/java/main/game/model/entity/usable/Usable.java
@@ -1,14 +1,10 @@
 package main.game.model.entity.usable;
 
-import static java.util.Objects.requireNonNull;
-
 import java.io.Serializable;
 import java.util.Collection;
-import main.game.model.entity.Unit;
-import main.game.model.entity.exceptions.CantApplyToUnitsException;
-import main.game.model.entity.exceptions.UsableStillInCoolDownException;
-import main.game.model.world.World;
 import main.common.images.GameImage;
+import main.game.model.entity.Unit;
+import main.game.model.world.World;
 
 /**
  * An usable {@link Item} or {@link Ability} - these have some effect on the unit (e.g. instant
@@ -17,29 +13,13 @@ import main.common.images.GameImage;
 public interface Usable extends Serializable {
 
   /**
-   * Creates an {@link Effect} for each unit through {@link Usable#_createEffectForUnit(Unit)} that
-   * this {@link Usable} selects through {@link Usable#_selectUnitsToApplyOn(World, Collection)} and
-   * applies the effects.
+   * Creates an {@link Effect} for each unit through {@link BaseUsable#_createEffectForUnit(Unit)}
+   * that this {@link Usable} selects through {@link BaseUsable#_selectUnitsToApplyOn(World,
+   * Collection)} and applies the effects.
    *
    * @throws IllegalStateException When this is not ready to be used yet (e.g. cool-down).
    */
-  default void use(World world, Collection<Unit> selectedUnits) {
-    if (!isReadyToBeUsed()) {
-      throw new UsableStillInCoolDownException();
-    }
-
-    Collection<Unit> unitsToApplyOn = _selectUnitsToApplyOn(
-        requireNonNull(world),
-        requireNonNull(selectedUnits)
-    );
-
-    for (Unit unit : unitsToApplyOn) {
-      Effect effect = _createEffectForUnit(unit);
-      unit.addEffect(effect);
-    }
-
-    _startCoolDown();
-  }
+  void use(World world, Collection<Unit> selectedUnits);
 
   /**
    * False if currently in a cool-down state.
@@ -70,26 +50,5 @@ public interface Usable extends Serializable {
    * 0 if just used, 1 if ready to use.
    */
   double getCoolDownProgress();
-
-  /**
-   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS!
-   * Pick what units to apply this ability to.
-   *
-   * @param selectedUnits The units that are currently selected by the user.
-   * @throws CantApplyToUnitsException When there is a unit that we cannot apply this {@link Usable}
-   *     to.
-   */
-  Collection<Unit> _selectUnitsToApplyOn(World world, Collection<Unit> selectedUnits);
-
-  /**
-   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS! Starts the cool-down period.
-   */
-  void _startCoolDown();
-
-  /**
-   * PROTECTED - DON"T CALL FROM OUTSIDE THIS CLASS! Creates a new effect. Does not need to check
-   * {@link Usable#isReadyToBeUsed()}.
-   */
-  Effect _createEffectForUnit(Unit unit);
 
 }
