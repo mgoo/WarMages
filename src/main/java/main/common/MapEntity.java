33a06fff70d0f77888a3631e0ca9e67172b6253e
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-10-12 16:56:46 +1300

    Changed definition of MapEntity

--- src/main/java/main/common/MapEntity.java
+++ src/main/java/main/common/MapEntity.java
@@ -3,7 +3,7 @@ package main.common;
 import main.common.util.MapPoint;
 
 /**
- * An {@link Entity} that cannot move / be moved on the map, and takes up a whole square {@link
+ * An {@link Entity} that cannot move / be moved on the map, and {@link
  * Unit}s cannot move through one of these.
  */
 public interface MapEntity extends Entity {
