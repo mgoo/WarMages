070987395ca1e3f5823c02e47a4c788eac90fe3a
Andrew McGhie <andrewmcghienz@gmail.com>
2017-10-12 21:15:37 +1300

    Removed load menu

--- src/main/java/main/menu/MenuFileResources.java
+++ src/main/java/main/menu/MenuFileResources.java
@@ -5,13 +5,11 @@ package main.menu;
  */
 public enum MenuFileResources {
   JQUERY_JS("resources/html/js/jquery-3.2.1.min.js"),
+  BOOTSTRAP_JS("resources/html/js/bootstrap.min.js"),
 
   HUD_HTML("resources/html/hud.html"),
   HUD_JS("resources/html/js/hud.js"),
   HUD_CSS("resources/html/css/hud.css"),
-
-  LOAD_MENU_HTML("resources/html/load_menu.html"),
-  LOAD_MENU_CSS("resources/html/css/load_menu.css"),
 
   MAIN_MENU_HTML("resources/html/main_menu.html"),
   MAIN_MENU_CSS("resources/html/css/main_menu.css");
