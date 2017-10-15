67ee0f316cbf9eb5ffa1b71da5bb8bf899e2a9dd
arorahrsh <arorahrsh@myvuw.ac.nz>
2017-10-15 20:11:27 +1300

    correct javadoc for ability

--- src/main/java/main/common/entity/usable/Ability.java
+++ src/main/java/main/common/entity/usable/Ability.java
@@ -4,7 +4,7 @@ import main.common.entity.Unit;
 import main.common.entity.Usable;
 
 /**
- * An ability can be applied to (a) HeroUnit(s).
+ * An ability can be applied to any units on the players team
  * @author chongdyla
  */
 public interface Ability extends Usable {
