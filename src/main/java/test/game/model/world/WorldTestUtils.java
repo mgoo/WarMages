6591e0bde15c875e0867060ecfa40c1f9894ea66
arorahrsh <arorahrsh@ecs.vuw.ac.nz>
2017-10-15 22:41:19 +1300

    added old stuff back

--- src/main/java/test/game/model/world/WorldTestUtils.java
+++ src/main/java/test/game/model/world/WorldTestUtils.java
@@ -1,6 +1,7 @@
 package test.game.model.world;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import main.common.entity.HeroUnit;
@@ -123,6 +124,13 @@ public class WorldTestUtils {
    */
   public static Ability createStubAbility() {
     return new BaseAbility("", GameImageResource.TEST_IMAGE_1_1.getGameImage(), 1, 2) {
+
+      @Override
+      public Collection<Unit> selectUnitsToApplyOn(
+          World world, Collection<Unit> selectedDefaultUnits
+      ) {
+        return Collections.emptyList();
+      }
 
       @Override
       public Effect createEffectForUnit(Unit unit) {
