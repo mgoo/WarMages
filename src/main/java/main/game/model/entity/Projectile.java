dfeb53d550aa6b45aa4569eea574f8f399069181
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-10-15 23:02:12 +1300

    Pulled projectile methods up to projectile interface and renamed concrete implementation to DefaultProjectile

--- src/main/java/main/game/model/entity/Projectile.java
+++ src/main/java/main/game/model/entity/Projectile.java
@@ -1,115 +1,40 @@
 package main.game.model.entity;
 
-import static java.util.Objects.requireNonNull;
 
-import main.common.entity.Entity;
-import main.common.entity.Unit;
 import main.common.World;
-import main.common.images.GameImage;
-import main.common.util.MapPoint;
-import main.common.util.MapSize;
+import main.common.entity.Entity;
 
 /**
  * Projectile extends {@link Entity}. A projectile is fired by a unit at a target (another unit) and
  * affects it in some way upon impact.
  * @author paladogabr
  */
-public class Projectile extends DefaultEntity {
-
-  private static final long serialVersionUID = 1L;
-  private static final double IMPACT_DISTANCE = 0.01;
-
-  private final double damageAmount;
-  private final Unit target;
-  private final Unit owner;
-  private final double moveDistancePerTick;
-
-  private boolean hasHit = false;
-  private GameImage image;
+public interface Projectile extends Entity {
 
   /**
-   * Constructor takes the starting coordinates of the projectile, the size,
-   * and the target of the projectile.
-   * @param coordinates at start of projectile path.
-   * @param size of projectile.
-   * @param target unit of projectile.
-   * @param owner the unit that fired the projectile
-   * @param gameImage image of the projectile.
-   * @param moveDistancePerTick distance to be moved per tick.
+   * Returns a boolean based on whether the Projectile has hit it's target or not.
+   *
+   * @return boolean hit target
    */
-  public Projectile(
-      MapPoint coordinates,
-      MapSize size,
-      Unit target,
-      Unit owner,
-      GameImage gameImage,
-      double moveDistancePerTick
-  ) {
-    super(coordinates, size);
-    this.target = requireNonNull(target);
-    this.owner = owner;
-    // So level ups and buffs do not effect fired projectile damage
-    this.damageAmount = owner.getDamageAmount();
-    this.moveDistancePerTick = moveDistancePerTick;
-    this.image = gameImage;
-  }
+  boolean hasHit();
 
-  @Override
-  public GameImage getImage() {
-    return image;
-  }
-
-  @Override
-  public void tick(long timeSinceLastTick, World world) {
-    if (hasHit) {
-      throw new IllegalStateException();
-    }
-
-    double distToTarget = getDistanceToTarget();
-    // 0.5 if we move halfway there, 1 or greater if we move all the way there, etc
-    double percentage = moveDistancePerTick / distToTarget;
-
-    if (percentage >= 1) {
-      percentage = 1; // teleport there because we are close enough
-    }
-
-    translatePosition(
-        percentage * (target.getCentre().x - getCentre().x),
-        percentage * (target.getCentre().y - getCentre().y)
-    );
-
-    if (getDistanceToTarget() <= IMPACT_DISTANCE) {
-      hasHit = true;
-      hitTarget(world);
-    }
-  }
-
-  @Override
-  public boolean contains(MapPoint point) {
-    return getRect().contains(point);
-  }
-
-  public boolean hasHit() {
-    return hasHit;
-  }
-
-  public double getDamageAmount() {
-    return damageAmount;
-  }
+  /**
+   * Returns the damage amount of the Projectile.
+   *
+   * @return double damage amount
+   */
+  double getDamageAmount();
 
   /**
    * Applies actions to given unit when it is hit by the Projectile.
    */
-  private void hitTarget(World world) {
-    boolean killed = target.takeDamage(damageAmount, world);
-    if (killed) {
-      this.owner.nextLevel();
-    }
-    world.removeProjectile(this);
-  }
+  void hitTarget(World world);
 
-  private double getDistanceToTarget() {
-    return getCentre().distanceTo(target.getCentre());
-  }
+  /**
+   * Returns the distance to the target
+   *
+   * @return double distance
+   */
+  double getDistanceToTarget();
 }
 
