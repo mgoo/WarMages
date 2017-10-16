e42908b9ca0b21b6b73fd0dab85914fc36ca8e59
Gabrielle Palado <paladogabr@ecs.vuw.ac.nz>
2017-10-16 15:31:25 +1300

    Added isSameTypeAs method in Unit interface, removed getType, and pulled methods up for DeadUnit to empty interface

---
+++ src/main/java/main/common/entity/DeadUnit.java
@@ -0,0 +1,12 @@
+package main.common.entity;
+
+import main.common.entity.MapEntity;
+
+/**
+ * DeadUnit MapEntity interface.
+ *
+ * @author paladogabr
+ */
+public interface DeadUnit extends MapEntity {
+
+}
